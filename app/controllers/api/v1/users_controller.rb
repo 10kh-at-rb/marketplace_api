8d8c2de2c1b4a2ec3adfe1ce786dd5223d083d29
Paul Riddle <paulriddle@openmailbox.org>
2015-02-22 02:34:10 +0500

    Implement updating users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,9 +6,14 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      render json: user, status: :created, location: [:api, user]
+    user = User.create(user_params)
+    respond_with user, status: :created, location: [:api, user]
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      render json: user, status: :ok, location: [:api, user]
     else
       render json: { errors: user.errors }, status: :unprocessable_entity
     end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -9,43 +9,76 @@ RSpec.describe Api::V1::UsersController do
       get :show, id: @user.id, format: :json
     end
 
-    it "returns the information about a reporter on a hash" do
+    it { should respond_with :ok }
+
+    it "returns user in json format" do
       user_response = JSON.parse(response.body, symbolize_names: true)
       expect(user_response[:email]).to eql @user.email
     end
-
-    it { should respond_with :ok }
   end
 
   describe "POST #create" do
-    context "when is successfully created" do
+    context "on success" do
       before(:example) do
         @user_attributes = Fabricate.attributes_for(:user)
-        post :create, { user: @user_attributes }, format: :json
-      end
-
-      it "renders the json representation for the user record just created" do
-        user_response = JSON.parse(response.body, symbolize_names: true)
-        expect(user_response[:email]).to eql(@user_attributes[:email])
+        post :create, user: @user_attributes, format: :json
       end
 
       it { should respond_with :created }
+
+      it "renders created user in json" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql(@user_attributes[:email])
+      end
     end
 
-    context "when is not created" do
+    context "on error" do
       before(:example) do
         @invalid_user_attributes = { password: "12345678",
                                      password_confirmation: "12345678" }
-        post :create, { user: @invalid_user_attributes }, format: :json
-        @user_response = JSON.parse(response.body, symbolize_names: true)
-      end
-
-      it "renders json with errors" do
-        expect(@user_response).to have_key(:errors)
-        expect(@user_response[:errors][:email]).to include("can't be blank")
+        post :create, user: @invalid_user_attributes, format: :json
       end
 
       it { should respond_with :unprocessable_entity }
+
+      it "renders json with errors" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+        expect(user_response[:errors][:email]).to include("can't be blank")
+      end
+    end
+  end
+
+  describe "PUT/PATCH #update" do
+    context "on success" do
+      before(:example) do
+        @user = Fabricate(:user)
+        patch :update, id: @user.id,
+                       user: { email: "user@example.io" }, format: :json
+      end
+
+      it { should respond_with :ok }
+
+      it "renders created user in json" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql "user@example.io"
+      end
+    end
+
+    context "on error" do
+      before(:example) do
+        @user = Fabricate(:user)
+        patch :update, id: @user.id,
+                       user: { email: "bademail.com" }, format: :json
+      end
+
+      it { should respond_with :unprocessable_entity }
+
+      it "renders json with errors" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+        expect(user_response[:errors][:email]).to include("is invalid")
+      end
     end
   end
 end
